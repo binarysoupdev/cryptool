6d24c2537dbd985d1e1fa543fd0e0efc5b56bd4c
binary_soup <contact@binarysoup.dev>
2026-01-23 12:21:41 -0500

    Clean up main

--- main.go
+++ main.go
@@ -7,10 +7,19 @@ import (
 )
 
 func main() {
+	key := "password123"
+
+	c, err := crypt.New(key)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+	}
+
 	plaintext := "foobar"
+	run(c, plaintext)
+	run(c, plaintext)
+}
 
-	c, err := crypt.New("password123")
-
+func run(c crypt.Crypt, plaintext string) {
 	ciphertext, err := c.Encrypt([]byte(plaintext))
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err)
