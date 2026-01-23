27be2d568b72c0473282acb5a569fda5d09aa4a5
binary_soup <contact@binarysoup.dev>
2026-01-23 12:31:12 -0500

    Add nonce (with error)

--- crypt/ops.go
+++ crypt/ops.go
@@ -1,19 +1,24 @@
 package crypt
 
-func (c Crypt) Encrypt(plaintext []byte) ([]byte, error) {
-	nonce := make([]byte, c.cipher.NonceSize())
+import "crypto/rand"
 
-	ciphertext := c.cipher.Seal(nil, nonce, plaintext, nil)
+func (c Crypt) Encrypt(plaintext []byte) ([]byte, error) {
+	ciphertext := c.cipher.Seal(nil, c.nonce(), plaintext, nil)
 	return ciphertext, nil
 }
 
 func (c Crypt) Decrypt(ciphertext []byte) ([]byte, error) {
-	nonce := make([]byte, c.cipher.NonceSize())
-
-	plaintext, err := c.cipher.Open(nil, nonce, ciphertext, nil)
+	plaintext, err := c.cipher.Open(nil, c.nonce(), ciphertext, nil)
 	if err != nil {
 		return nil, chainError(err, "error decryting ciphertext")
 	}
 
 	return plaintext, nil
+}
+
+func (c Crypt) nonce() []byte {
+	nonce := make([]byte, c.cipher.NonceSize())
+	rand.Read(nonce)
+
+	return nonce
 }
