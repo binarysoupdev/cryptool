11f237970c9e460777126bbb92ff40bb7d3302e5
binary_soup <contact@binarysoup.dev>
2026-02-12 19:02:50 -0500

    Add headers to crypt tests

--- crypt/operations_test.go
+++ crypt/operations_test.go
@@ -10,28 +10,34 @@ import (
 )
 
 func TestEncryptDecryptCorrectKey(t *testing.T) {
+	//-- arrange
 	r := rand.New(1)
 	password := r.ASCII(30)
 	plaintext := r.Bytes(100)
 
+	//-- act
 	c, _ := crypt.New(password)
 	res, err := c.Decrypt(c.Encrypt(plaintext))
 
+	//-- assert
 	require.NoError(t, err)
 	assert.Equal(t, plaintext, res, "plaintext do not match")
 }
 
 func TestEncryptDecryptWrongKey(t *testing.T) {
+	//-- arrange
 	r := rand.New(2)
 	password := r.ASCII(30)
 	plaintext := r.Bytes(100)
 
+	//-- act
 	c, salt := crypt.New(password)
 	ciphertext := c.Encrypt(plaintext)
 
 	c = crypt.Load("", salt)
 	_, err := c.Decrypt(ciphertext)
 
+	//-- assert
 	require.Error(t, err)
 	assert.Contains(t, err.Error(), "error decrypting ciphertext")
 }
