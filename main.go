342bbbc75af8ec79c45eaca5ccf4f67b87e839f4
binary_soup <contact@binarysoup.dev>
2026-02-27 20:15:32 -0500

    Run crypt in main

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/binarysoupdev/cryptool/crypt"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	c := crypt.New()
+
+	ciphertext := c.Encrypt([]byte("lorem ipsum"))
+	fmt.Println(ciphertext)
+
+	plaintext, err := c.Decrypt(ciphertext)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(plaintext))
 }
