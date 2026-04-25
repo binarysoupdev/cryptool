ecc1123b1165f8da8471aa1d12be801fa4431596
binary_soup <contact@binarysoup.dev>
2026-04-25 14:52:48 -0400

    Implement nonce

--- crypt/operations.go
+++ crypt/operations.go
@@ -1,13 +1,14 @@
 package crypt
 
+import "crypto/rand"
+
 func (c Crypt) Encrypt(plaintext []byte) []byte {
 	nonce := make([]byte, c.cipher.NonceSize())
+	rand.Read(nonce)
 
-	return c.cipher.Seal(nil, nonce, plaintext, nil)
+	return c.cipher.Seal(nonce, nonce, plaintext, nil)
 }
 
 func (c Crypt) Decrypt(ciphertext []byte) ([]byte, error) {
-	nonce := make([]byte, c.cipher.NonceSize())
-
-	return c.cipher.Open(nil, nonce, ciphertext, nil)
+	return c.cipher.Open(nil, ciphertext[:c.cipher.NonceSize()], ciphertext[c.cipher.NonceSize():], nil)
 }
