75fbc8e201acc487b7c57c9d08aaaee796bd63f1
binary_soup <contact@binarysoup.dev>
2026-08-06 20:25:10 -0400

    Allow persistent host

--- internal/app/host.go
+++ internal/app/host.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 
+	"github.com/binarysoupdev/cryptool/net/conn"
 	"github.com/binarysoupdev/cryptool/net/host"
 	"github.com/binarysoupdev/go-commando/command"
 	"github.com/binarysoupdev/go-extensions/errors"
@@ -29,19 +30,43 @@ func (cmd HostCommand) Run(args []string) error {
 	}
 	defer host.Close()
 
-	style.BoldInfo.Printf("Listening at: %s\n", host.Addr())
+	cmd.acceptLoop(host)
+	return nil
+}
 
-	c, err := host.Accept()
+func (cmd HostCommand) acceptLoop(h host.Host) {
+	style.BoldInfo.Printf("Listening at: %s\n", h.Addr())
+	for {
+		err := cmd.acceptClient(h)
+		if err != nil {
+			cmd.logError(err)
+		}
+	}
+}
+
+func (cmd HostCommand) acceptClient(h host.Host) error {
+	c, err := h.Accept()
 	if err != nil {
 		return errors.Chain(err, "error accepting connection")
 	}
-	style.Create.Printf("Accepted Client: %s\n", c.RemoteAddr())
 
+	style.Create.Printf("Accepted Client: %s\n", c.RemoteAddr())
+	err = cmd.accept(c)
+	style.Delete.Printf("Ended Client: %s\n", c.RemoteAddr())
+
+	return err
+}
+
+func (cmd HostCommand) accept(c *conn.Conn) error {
 	msg, err := c.ReadMessage()
 	if err != nil {
 		return errors.Chain(err, "error reading message")
 	}
-	fmt.Printf("Received message: %s\n", string(msg))
 
+	fmt.Printf("Received message: %s\n", string(msg))
 	return nil
+}
+
+func (cmd HostCommand) logError(err error) {
+	fmt.Printf("%s %s\n", style.BoldError.Sprint("[X] "), err)
 }
