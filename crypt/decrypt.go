4ca4e6394a3975252d73e7624d33d89b0f656a5a
binary_soup <contact@binarysoup.dev>
2026-08-14 20:34:10 -0400

    Clean up crypt package

--- crypt/decrypt.go
+++ crypt/decrypt.go
@@ -1,19 +1,8 @@
 package crypt
 
 import (
-	"crypto/rand"
 	"fmt"
 )
-
-const NONCE_SIZE = 12 // AES-GCM
-
-// Encrypt the plaintext and return the resulting ciphertext.
-func (c Crypt) Encrypt(plaintext []byte) []byte {
-	nonce := make([]byte, NONCE_SIZE)
-	rand.Read(nonce)
-
-	return c.cipher.Seal(nonce, nonce, plaintext, nil)
-}
 
 // Decrypt the ciphertext and return the resulting plaintext. Also returns any decryption errors.
 func (c Crypt) Decrypt(ciphertext []byte) ([]byte, error) {
